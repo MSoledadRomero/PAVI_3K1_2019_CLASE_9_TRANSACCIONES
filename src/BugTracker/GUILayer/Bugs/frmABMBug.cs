e4c1107eac2d90fda66d875a03675183a30cfca4
MSoledadRomero <romeroma.soledad@gmail.com>
2019-10-07 03:32:47 -0300

    ejemplo transacciones insert bug + insert historico

---
+++ src/BugTracker/GUILayer/Bugs/frmABMBug.cs
@@ -0,0 +1,170 @@
+﻿using BugTracker.BusinessLayer;
+using BugTracker.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BugTracker.GUILayer.Bugs
+{
+    public partial class frmABMBug : Form
+    {
+
+        private PrioridadService prioridadService;
+        private ProductoService productoService;
+        private CriticidadService criticidadService;
+
+        private Bug oBugSeleccionado;
+        private BugService oBugService;
+        private FormMode formMode = FormMode.insert;
+
+        public enum FormMode
+        {
+            insert,
+            update,
+            delete
+        }
+        public frmABMBug()
+        {
+            InitializeComponent();
+
+            oBugService = new BugService();
+            criticidadService = new CriticidadService();
+            productoService = new ProductoService();
+            prioridadService = new PrioridadService();
+
+        }
+
+        public void InicializarFormulario(FormMode op, Bug bugSeleccionado)
+        {
+            formMode = op;
+            oBugSeleccionado = bugSeleccionado;
+        }
+
+        private void FrmABMBug_Load(object sender, EventArgs e)
+        {
+            LlenarCombo(cboPrioridad, prioridadService.ObtenerTodos(), "Nombre", "IdPrioridad");
+            LlenarCombo(cboCriticidad, criticidadService.ObtenerTodos(), "Nombre", "IdCriticidad");
+            LlenarCombo(cboProducto, productoService.ObtenerTodos(), "Nombre", "IdProducto");
+            switch (formMode)
+            {
+                case FormMode.insert:
+                    {
+                        this.Text = "Nuevo Bug";
+                        break;
+                    }
+
+                case FormMode.update:
+                    {
+                        this.Text = "Actualizar Bug";
+                        // Recuperar Bug seleccionado en la grilla 
+                        MostrarDatos();
+                        txtTitulo.Enabled = true;
+                        txtDescripcion.Enabled = true;
+                        cboPrioridad.Enabled = true;
+                        cboCriticidad.Enabled = true;
+                        cboProducto.Enabled = true;
+                        break;
+                    }
+
+            }
+
+
+        }
+
+        private void MostrarDatos()
+        {
+            if (oBugSeleccionado != null)
+            {
+                txtTitulo.Text = oBugSeleccionado.Titulo;
+                txtDescripcion.Text = oBugSeleccionado.Descripcion;
+
+                cboPrioridad.SelectedValue = oBugSeleccionado.Prioridad.IdPrioridad;
+                cboCriticidad.SelectedValue = oBugSeleccionado.Criticidad.IdCriticidad;
+                cboProducto.SelectedValue = oBugSeleccionado.Producto.IdProducto;
+            }
+        }
+
+        private void LlenarCombo(ComboBox cbo, Object source, string display, String value)
+        {
+            cbo.DataSource = source;
+            cbo.DisplayMember = display;
+            cbo.ValueMember = value;
+            cbo.SelectedIndex = -1;
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                switch (formMode)
+                {
+                    case FormMode.insert:
+                        {
+                            if (ValidarCampos())
+                            {
+                                var oBug = new Bug();
+                                oBug.Titulo = txtTitulo.Text;
+                                oBug.Descripcion = txtDescripcion.Text;
+                                oBug.Prioridad = new Prioridad();
+                                oBug.Prioridad.IdPrioridad = (int)cboPrioridad.SelectedValue;
+                                oBug.Criticidad = new Criticidad();
+                                oBug.Criticidad.IdCriticidad = (int)cboCriticidad.SelectedValue;
+                                oBug.Producto = new Producto();
+                                oBug.Producto.IdProducto = (int)cboProducto.SelectedValue;
+
+                                if (oBugService.crearBugconHistorial(oBug))
+                                {
+                                    MessageBox.Show("Bug con historial insertado!", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    this.Close();
+                                }
+                            }
+
+                            break;
+                        }
+
+                    case FormMode.update:
+                        {
+                            if (ValidarCampos())
+                            {
+                                oBugSeleccionado.Titulo = txtTitulo.Text;
+                                oBugSeleccionado.Descripcion = txtDescripcion.Text;
+
+                                oBugSeleccionado.Prioridad.IdPrioridad = (int)cboPrioridad.SelectedValue;
+                                oBugSeleccionado.Criticidad.IdCriticidad = (int)cboCriticidad.SelectedValue;
+                                oBugSeleccionado.Producto.IdProducto = (int)cboProducto.SelectedValue;
+
+                             // if (oBugService.ActualizarBug(oBugSeleccionado))
+                               // {
+                              //      MessageBox.Show("Bug actualizado!", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                               //     this.Dispose();
+                               // }
+                            //    else
+                             //       MessageBox.Show("Error al actualizar el Bug!", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                           }
+
+                            break;
+                        }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar el usuario! " + ex.Message + ex.InnerException.Message + ex.StackTrace, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private bool ValidarCampos()
+        {
+            return true;
+        }
+    }
+}
---
+++ src/BugTracker/GUILayer/Bugs/frmABMBug.designer.cs
@@ -0,0 +1,190 @@
+﻿namespace BugTracker.GUILayer.Bugs
+{
+    partial class frmABMBug
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCriticidad = new System.Windows.Forms.ComboBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.cboPrioridad = new System.Windows.Forms.ComboBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.cboProducto = new System.Windows.Forms.ComboBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cboCriticidad
+            // 
+            this.cboCriticidad.FormattingEnabled = true;
+            this.cboCriticidad.Location = new System.Drawing.Point(87, 165);
+            this.cboCriticidad.Name = "cboCriticidad";
+            this.cboCriticidad.Size = new System.Drawing.Size(254, 21);
+            this.cboCriticidad.TabIndex = 35;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(34, 168);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(51, 13);
+            this.Label4.TabIndex = 42;
+            this.Label4.Text = "Prioridad:";
+            // 
+            // cboPrioridad
+            // 
+            this.cboPrioridad.FormattingEnabled = true;
+            this.cboPrioridad.Location = new System.Drawing.Point(87, 141);
+            this.cboPrioridad.Name = "cboPrioridad";
+            this.cboPrioridad.Size = new System.Drawing.Size(254, 21);
+            this.cboPrioridad.TabIndex = 34;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(32, 144);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(53, 13);
+            this.Label3.TabIndex = 41;
+            this.Label3.Text = "Criticidad:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(87, 42);
+            this.txtDescripcion.MaxLength = 1000;
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(254, 72);
+            this.txtDescripcion.TabIndex = 32;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(194, 224);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(80, 23);
+            this.btnCancelar.TabIndex = 37;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(110, 224);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(78, 23);
+            this.btnAceptar.TabIndex = 36;
+            this.btnAceptar.Text = "&Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // cboProducto
+            // 
+            this.cboProducto.FormattingEnabled = true;
+            this.cboProducto.Location = new System.Drawing.Point(87, 117);
+            this.cboProducto.Name = "cboProducto";
+            this.cboProducto.Size = new System.Drawing.Size(254, 21);
+            this.cboProducto.TabIndex = 33;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(22, 121);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(63, 13);
+            this.Label5.TabIndex = 40;
+            this.Label5.Text = "Producto(*):";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(18, 41);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(66, 13);
+            this.Label2.TabIndex = 39;
+            this.Label2.Text = "Descripción:";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(38, 22);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(46, 13);
+            this.Label1.TabIndex = 38;
+            this.Label1.Text = "Titulo(*):";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(87, 19);
+            this.txtTitulo.MaxLength = 100;
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(254, 20);
+            this.txtTitulo.TabIndex = 31;
+            // 
+            // frmABMBug
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 272);
+            this.Controls.Add(this.cboCriticidad);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.cboPrioridad);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.cboProducto);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.txtTitulo);
+            this.Name = "frmABMBug";
+            this.Text = "Actualizar Bug";
+            this.Load += new System.EventHandler(this.FrmABMBug_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.ComboBox cboCriticidad;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.ComboBox cboPrioridad;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox txtDescripcion;
+        internal System.Windows.Forms.Button btnCancelar;
+        internal System.Windows.Forms.Button btnAceptar;
+        internal System.Windows.Forms.ComboBox cboProducto;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox txtTitulo;
+    }
+}
